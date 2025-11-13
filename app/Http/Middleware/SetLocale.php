991cfbd9a263d1f4381566ae9232a4e61cd98e8b
Mehmet Ali ARKUN <maliarkun@gmail.com>
2025-11-14 02:08:57 +0300

    Fix SetLocale middleware

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -5,20 +5,20 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Session;
 
 class SetLocale
 {
     public function handle(Request $request, Closure $next)
     {
-        $locale = $request->get('lang') ?? Session::get('app_locale', config('app.locale'));
+        // URL parametresi -> session -> varsayılan app.locale sıralaması
+        $locale = $request->get('lang', session('locale', config('app.locale')));
 
-        if (! in_array($locale, ['en', 'tr'])) {
+        if (! in_array($locale, ['tr', 'en'])) {
             $locale = config('app.locale');
         }
 
         App::setLocale($locale);
-        Session::put('app_locale', $locale);
+        session()->put('locale', $locale);
 
         return $next($request);
     }
